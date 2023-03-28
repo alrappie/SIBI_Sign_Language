188d3c6b689d1b8799afb21e9cc663c8191e1f1e
Alrappie <alravimahesa@student.ub.ac.id>
2023-03-28 16:11:14 +0700

    add train

--- hand_sign.py
+++ hand_sign.py
@@ -3,12 +3,19 @@ import time
 import numpy as np
 import os
 import mediapipe as mp
+import tensorflow as tf
+from keras.preprocessing import image
 
 mpHands = mp.solutions.hands
-hands = mpHands.Hands()
+hands = mpHands.Hands(max_num_hands=1)
 mpDraw = mp.solutions.drawing_utils
-offset = 20
 cap = cv2.VideoCapture(0)
+model = tf.keras.models.load_model('my_model3.h5')
+
+
+FOLDER = './myData/D'
+counter = 0
+dict_list = {0:'A',1:'B',2:'C',3:'D',4:'E'}
 
 while True:
     sucess,img = cap.read()
@@ -18,8 +25,6 @@ while True:
     results = hands.process(imgRGB)
     if results.multi_hand_landmarks:
         for hand_landmark in results.multi_hand_landmarks:
-            lm_x = []
-            lm_y = []
             x_max = 0
             y_max = 0
             x_min = w
@@ -37,14 +42,17 @@ while True:
             
             mpDraw.draw_landmarks(img,hand_landmark,mpHands.HAND_CONNECTIONS)
                                     
-            cv2.putText(img,'Hand',(x_min-25,y_min-25),cv2.FONT_HERSHEY_COMPLEX,1,(0,0,0),1)
+            
+            ## bounding box
+            
+            #Rectangle
             cv2.rectangle(img, (x_min-20, y_min-20), (x_max+20, y_max+20), (121, 9, 129), 2)
             
-            # top left
+            #top left
             cv2.line(img,(x_min-20,y_min+30),(x_min-20,y_min-20),(255,0,255),5)
             cv2.line(img,(x_min-20,y_min-20),(x_min+30,y_min-20),(255,0,255),5)
 
-            # top right
+            #top right
             cv2.line(img,(x_max-30,y_min-20),(x_max+20,y_min-20),(255,0,255),5)
             cv2.line(img,(x_max+20,y_min-20),(x_max+20,y_min+30),(255,0,255),5)
             
@@ -59,15 +67,28 @@ while True:
             # print()
             # print(x_max-x_min)
             if ((y_min>20) & (x_min>20)):
-                imgCrop = img[y_min-20:y_max+20,x_min-20:x_max+20]
-                cv2.imshow('Image2',imgCrop)
                 
-            print(y_min,x_min)
-            # imgCropShape = imgCrop.shape
-            # img[0:imgCropShape[0],0:imgCropShape[1]] = imgCrop
-            # print(imgCrop.shape)
-            # print(f'xmin: {x_min}, xmax: {x_max}, ymin: {y_min}, ymax: {y_max}, width: {x_max+x_min}, heigth: {y_max+y_min}')
-            
-        
+                img_crop = img[y_min-20:y_max+20,x_min-20:x_max+20]
+                
+                cv2.imshow('Image2',img_crop)
+                    
+                ## do preprocessing
+                img_crop = cv2.resize(img_crop,(200,200),cv2.INTER_AREA)
+                img_crop = image.img_to_array(img_crop)
+                img_crop = np.expand_dims(img_crop, axis=0)
+                img_crop /= 255.
+                
+                # Predict
+                pred = np.argmax(model.predict(img_crop))
+                
+                cv2.putText(img,f'{dict_list[pred]}',(x_min-25,y_min-25),cv2.FONT_HERSHEY_COMPLEX,1,(0,0,0),1)
+                
+                
+                
     cv2.imshow("Image",img)
-    cv2.waitKey(1)
+    key = cv2.waitKey(1)
+    if key == ord('s'):
+        counter +=1
+        print(img_crop.shape)
+        cv2.imwrite(f'{FOLDER}/Image_{counter}.jpg',img_crop)
+        print(counter)
